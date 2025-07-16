4c473564823b8b8f44f9bcc1a78fc0e0a814f754
XuWei <xiaohuoma7620@163.com>
2025-07-16 15:33:47 +0800

    S006增加TTF

--- SDL2_Sample/S006_SoundAndExtensionLibraries/S006_SoundAndExtensionLibraries.cpp
+++ SDL2_Sample/S006_SoundAndExtensionLibraries/S006_SoundAndExtensionLibraries.cpp
@@ -3,6 +3,7 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+#include <SDL_ttf.h>
 
 using namespace std;
 
@@ -10,12 +11,16 @@ bool init();
 void kill();
 bool loop();
 
-// Pointers to our window, renderer, texture, music, and sound
+// Pointers to our window, renderer, texture, music, sound and ttf
 SDL_Window* window;
 SDL_Renderer* renderer;
-SDL_Texture* texture;
+SDL_Texture* bg_img_texture;
 Mix_Music* music;
 Mix_Chunk* sound;
+
+TTF_Font* font; // 字体
+SDL_Texture* ttf_texture; // 字体纹理
+SDL_Rect ttf_rect; // 字体位置
 
 int main(int argc, char** args)
 {
@@ -67,8 +72,11 @@ bool loop()
         }
     }
 
-    // Render texture
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    // Render bg_img_texture
+    SDL_RenderCopy(renderer, bg_img_texture, NULL, NULL);
+
+    // 绘制ttf纹理
+    SDL_RenderCopy(renderer, ttf_texture, NULL, &ttf_rect);
 
     // Update window
     SDL_RenderPresent(renderer);
@@ -96,6 +104,14 @@ bool init()
         return false;
     }
 
+    // 初始化 SDL_ttf
+    if (TTF_Init() == -1) {
+        cout << "SDL_ttf could not initialize: " << TTF_GetError() << endl;
+        system("pause");
+        return false;
+    }
+
+    // 创建窗口
     window = SDL_CreateWindow("Example",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
         640, 480, SDL_WINDOW_SHOWN);
@@ -104,6 +120,7 @@ bool init()
         return false;
     }
 
+    // 创建渲染器
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
         cout << "Error creating renderer: " << SDL_GetError() << endl;
@@ -117,13 +134,13 @@ bool init()
         return false;
     }
 
-    // Create texture
-    texture = SDL_CreateTextureFromSurface(renderer, buffer);
+    // Create bg_img_texture
+    bg_img_texture = SDL_CreateTextureFromSurface(renderer, buffer);
     // Free surface as it's no longer needed
     SDL_FreeSurface(buffer);
     buffer = NULL;
-    if (!texture) {
-        cout << "Error creating texture: " << SDL_GetError() << endl;
+    if (!bg_img_texture) {
+        cout << "Error creating bg_img_texture: " << SDL_GetError() << endl;
         return false;
     }
 
@@ -144,18 +161,55 @@ bool init()
     // Play music forever
     Mix_PlayMusic(music, -1);
 
+    // 加载字体 - 替换为你的中文字体文件路径
+    const char* FONT_PATH = "STCAIYUN.TTF"; // 华文彩云，支持中文
+    const int FONT_SIZE = 72;
+    font = TTF_OpenFont(FONT_PATH, FONT_SIZE);
+    if (!font) {
+        cout << "Failed to load font: " << TTF_GetError() << endl;
+        return false;
+    }
+    // 设置字体样式
+    TTF_SetFontStyle(font, TTF_STYLE_BOLD);
+    TTF_SetFontOutline(font, 1); // 设置1像素轮廓
+    // 创建文字表面
+    SDL_Color font_color = { 255, 0, 255, 255 }; // RGBA紫色
+    SDL_Surface* blendedSurface = TTF_RenderUNICODE_Blended(font, (Uint16*)L"~ ABC主菜单 ~", font_color);
+    if (!blendedSurface) {
+        cout << "Failed to create blended text surface: " << TTF_GetError() << endl;
+        return false;
+    }
+    // 创建字体纹理
+    ttf_texture = SDL_CreateTextureFromSurface(renderer, blendedSurface);
+    if (!ttf_texture) {
+        cout << "Failed to create blended texture: " << TTF_GetError() << endl;
+        return false;
+    }
+    // 设置文本位置
+    ttf_rect = { 50, 50, blendedSurface->w, blendedSurface->h };
+    // 释放表面（不再需要）
+    SDL_FreeSurface(blendedSurface);
+
     return true;
 }
 
 void kill()
 {
-    SDL_DestroyTexture(texture);
+    // 销毁字体相关资源
+    SDL_DestroyTexture(ttf_texture);
+    TTF_CloseFont(font);
+
+    // 销毁背景图像相关资源
+    SDL_DestroyTexture(bg_img_texture);
+    bg_img_texture = NULL;
+
+    // 销毁声音相关资源
     Mix_FreeMusic(music);
     Mix_FreeChunk(sound);
-    texture = NULL;
     music = NULL;
     sound = NULL;
 
+    // 销毁窗体相关资源
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     window = NULL;
